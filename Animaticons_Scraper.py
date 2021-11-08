5e0e5424f71cfb467ea3493d31cf9c2099852cbd
Vossi <vossi@pop-os.localdomain>
2021-11-08 20:01:45 +0100

    Use array of urls
    Allow the scraping of multiple pages

--- Animaticons_Scraper.py
+++ Animaticons_Scraper.py
@@ -1,24 +1,22 @@
-# Website: https://animaticons.co/downloads/animaticons-camp/
+# Website: https://animaticons.co/downloads/
 
 import re
 
 import requests
 from bs4 import BeautifulSoup as bS
 
-# website to be scraped
-baseUrl = 'https://animaticons.co/downloads/animaticons-camp/'
 # Local download directory
 download_dir = '/home/vossi/Downloads/'
-# Limit the maximum number of downloads. None = download everything
+# Limit the maximum number of downloads per URL. None = download everything
 maxDownloads = None
 
 
-def scrape():
+def scrape(input_url, j):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
     }
 
-    html_txt = requests.get(baseUrl, headers=headers).text
+    html_txt = requests.get(input_url, headers=headers).text
     soup = bS(html_txt, 'lxml')
     elements = soup.find_all('li', {'style': re.compile(r'gif')})
 
@@ -28,13 +26,13 @@ def scrape():
     max_iters = len(elements) if maxDownloads is None else maxDownloads
 
     for i, element in enumerate(elements):
-        url = re.search(pattern, str(element)).group()
-        print(f'Download {i + 1} / {max_iters} from: {url}')
+        input_url = re.search(pattern, str(element)).group()
+        print(f'Download {i + 1} / {max_iters} from: {j} {input_url}')
 
-        request = requests.get(url, headers=headers)
+        request = requests.get(input_url, headers=headers)
 
-        last_slash = url.rfind('/')
-        file_name = url[last_slash + 1:-4]
+        last_slash = input_url.rfind('/')
+        file_name = input_url[last_slash + 1:-4]
         print(f"Status code: {request.status_code}; Headers: {request.headers['content-type']}; "
               f"Encoding: {request.encoding}; File name: {file_name}")
 
@@ -46,4 +44,15 @@ def scrape():
 
 
 if __name__ == '__main__':
-    scrape()
+    # websites to be scraped
+    baseUrls = ['https://animaticons.co/downloads/animaticons-essential-3/',
+                'https://animaticons.co/downloads/animaticons-designer/',
+                'https://animaticons.co/downloads/essential-plus/',
+                'https://animaticons.co/downloads/essential/',
+                'https://animaticons.co/downloads/animaticons-camp/']
+    for j, url in enumerate(baseUrls):
+        print(f'Scraping URL {j + 1} / {len(baseUrls)}. {url}')
+        scrape(url, j + 1)
+
+    print(" ")
+    print("Done")
